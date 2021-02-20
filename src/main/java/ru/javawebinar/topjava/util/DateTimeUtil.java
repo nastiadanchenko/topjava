f2a11f9ac1fe1518a7fe64fa797ca46ce7d6d7d0
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-02-21 01:01:13 +0500

    3 06 HW2 optional filter

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -29,4 +29,14 @@ public class DateTimeUtil {
 
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
+
+    public static @Nullable
+    LocalDate parseLocalDate(@Nullable String str) {
+        return StringUtils.hasLength(str) ? LocalDate.parse(str) : null;
+    }
+
+    public static @Nullable
+    LocalTime parseLocalTime(@Nullable String str) {
+        return StringUtils.hasLength(str) ? LocalTime.parse(str) : null;
+    }
 }
