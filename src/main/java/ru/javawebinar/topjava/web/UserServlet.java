e335dde482499de1e7e75d984aacf0d75a94bf5c
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-02-21 22:43:27 +0500

    3 07 HW2 optional select user

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -23,6 +23,14 @@ public class UserServlet extends HttpServlet {
     }
 
     @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // достаем ID
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        response.sendRedirect("meals");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to users");
         request.setAttribute("users",repository.getAll());
