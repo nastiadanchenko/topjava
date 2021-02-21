490eeafb5bc2d557c0b0b98621cd7d8d0a5e7251
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-02-21 22:42:20 +0500

    3 03 HW2 meal layers

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.util.ValidationUtil;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -20,11 +22,11 @@ public class MealService {
 
 
     public Meal get(int id, int userId) {
-        return repository.get(id, userId);
+        return ValidationUtil.checkNotFoundWithId(repository.get(id, userId),id);
     }
 
     public void delete(int id, int userId) {
-        repository.delete(id, userId);
+        ValidationUtil.checkNotFoundWithId(repository.delete(id, userId),id);
     }
 
     public List<Meal> getAll(int userId) {
@@ -36,10 +38,10 @@ public class MealService {
     }
 
     public void update(Meal meal, int userId) {
-        repository.save(meal, userId);
+        ValidationUtil.checkNotFoundWithId(repository.save(meal, userId),meal.getId());
     }
 
-    public List<Meal> getBetweenHalfOpen(LocalDate startDate, LocalDate endDate, int userId) {
+    public List<Meal> getBetweenHalfOpen(@Nullable LocalDate startDate, @Nullable LocalDate endDate, int userId) {
         return repository.getBetweenHalfOpen(DateTimeUtil.getStartInclusive(startDate), DateTimeUtil.getEndExclusive(endDate), userId);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
@@ -18,10 +19,14 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
-    private MealService service;
+    private final MealService service;
 @Autowired
     public MealRestController(MealService service) {
         this.service = service;
