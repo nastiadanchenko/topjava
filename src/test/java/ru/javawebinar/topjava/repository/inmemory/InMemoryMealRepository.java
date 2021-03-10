b4728d0569f89866913aa2b171879e2cec5aabe5
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-03-10 14:19:54 +0500

    3 15 fix servlet

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -8,8 +8,8 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.util.Util;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.*;
@@ -39,12 +39,12 @@ public class InMemoryMealRepository implements MealRepository {
         return meals.save(meal);
     }
 
-    @PostConstruct
+   // @PostConstruct
     public void postConstruct() {
         log.info("+++ PostConstruct");
     }
 
-    @PreDestroy
+   // @PreDestroy
     public void preDestroy() {
         log.info("+++ PreDestroy");
     }
