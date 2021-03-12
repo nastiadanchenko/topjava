a25671019a578bd8d909ff58209499a7c2cbe9b1
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-03-12 14:13:40 +0500

    4 11 fix validation

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -1,5 +1,13 @@
 package ru.javawebinar.topjava.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
 public abstract class AbstractNamedEntity extends AbstractBaseEntity {
 
     @NotBlank
